64db92037878434e5823c365f1f6c93f1f1f34bf
n-hassan01 <naimul.hassan.se@gmail.com>
2023-11-01 15:04:40 +0600

    update

---
+++ src/layouts/dashboard/nav/adminConfig.js
@@ -0,0 +1,76 @@
+// component
+import SvgColor from '../../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+// const navConfig = [
+//   {
+//     title: 'dashboard',
+//     path: '/dashboard/app',
+//     icon: icon('ic_analytics'),
+//   },
+//   {
+//     title: 'users',
+//     path: '/dashboard/user',
+//     icon: icon('ic_users'),
+//   },
+//   {
+//     title: 'portfolio',
+//     path: '/dashboard/portfolio',
+//     icon: icon('portfolio'),
+//   },
+//   {
+//     title: 'blog',
+//     path: '/dashboard/blog',
+//     icon: icon('ic_blog'),
+//   },
+//   {
+//     title: 'profile',
+//     path: '/dashboard/profile',
+//     icon: icon('ic_profile'),
+//   },
+//   {
+//     title: 'contact',
+//     path: '/dashboard/contact',
+//     icon: icon('ic_contact'),
+//   },
+// ];
+
+const navConfigAdmin = [
+  {
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'users',
+    path: '/dashboard/user',
+    icon: icon('ic_users'),
+  },
+  // {
+  //   title: 'portfolio',
+  //   path: '/dashboard/portfolio',
+  //   icon: icon('portfolio'),
+  // },
+  {
+    title: 'blog',
+    path: '/dashboard/blog',
+    icon: icon('ic_blog'),
+  },
+  // {
+  //   title: 'profile',
+  //   path: '/dashboard/profile',
+  //   icon: icon('ic_profile'),
+  // },
+  // {
+  //   title: 'contact',
+  //   path: '/dashboard/contact',
+  //   icon: icon('ic_contact'),
+  // },
+];
+
+// const selectedNav = role === 1 ? navConfigAdmin : navConfig
+
+export default navConfigAdmin;
--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 // @mui
-import { Avatar, Box, Drawer, Link, Typography } from '@mui/material';
+import { Avatar, Box, Drawer, Link, Stack, Typography } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
 // mock
 // hooks
@@ -11,6 +11,8 @@ import useResponsive from '../../../hooks/useResponsive';
 import NavSection from '../../../components/nav-section';
 import Scrollbar from '../../../components/scrollbar';
 //
+import { getLoggedInUserDetails } from '../../../Services/ApiServices';
+import navConfigAdmin from './adminConfig';
 import navConfig from './config';
 
 // ----------------------------------------------------------------------
@@ -44,6 +46,24 @@ export default function Nav({ openNav, onCloseNav }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
+  const [loggedInUser, setLoggedInUser] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const usersDetails = await getLoggedInUserDetails();
+        if (usersDetails) setLoggedInUser(usersDetails.data);
+      } catch (error) {
+        console.error('Error fetching account details:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(loggedInUser);
+  const selectedNav = loggedInUser.role === 1 ? navConfigAdmin : navConfig;
+
   const renderContent = (
     <Scrollbar
       sx={{
@@ -53,7 +73,7 @@ export default function Nav({ openNav, onCloseNav }) {
     >
       <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }} />
 
-      <Box sx={{ mb: 5, mx: 2.5 }}>
+      <Box sx={{ mb: 2, mx: 2.5 }}>
         <Link underline="none">
           <StyledAccount>
             <Avatar src={'/favicon/logo.png'} alt="logo" />
@@ -67,10 +87,22 @@ export default function Nav({ openNav, onCloseNav }) {
         </Link>
       </Box>
 
-      <NavSection data={navConfig} />
+      <NavSection data={selectedNav} />
 
       <Box sx={{ flexGrow: 1 }} />
 
+      <Box sx={{ mb: 2, mx: 1.5 }}>
+        <Stack align="center" alignItems="center" justifyContent="space-between">
+          <Typography variant="body2" color="text.secondary">
+            {'Copyright © '}
+            <Link color="inherit" target="_blank" href="https://n-hassan01.github.io/PortfolioWebsite/">
+              Naimul Hassan
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Stack>
+      </Box>
     </Scrollbar>
   );
 
